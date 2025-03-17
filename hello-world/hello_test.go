ced9ac7bacce5e55f5e4949bba71f7104361f604
maddawik <edwardreillycomputers@gmail.com>
2025-03-17 14:14:39 -0400

    Capture more requirements in a new test
    We're adding subtests here to group some tests together.
    
    In this case we're add a default case, that if an empty string is supplied we
    want to return 'Hello, world!'. We first add a test in this commit!
    
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world#hello-world...-again

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("EJ")
-	want := "Hello, EJ!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("EJ")
+		want := "Hello, EJ!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, world!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
