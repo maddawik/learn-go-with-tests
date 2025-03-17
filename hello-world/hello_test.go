53b3995cf507b8fb63e2e630e74f7cd100f647a4
maddawik <edwardreillycomputers@gmail.com>
2025-03-17 15:03:41 -0400

    Capture requirements for French
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world#french

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -20,6 +20,12 @@ func TestHello(t *testing.T) {
 		want := "Hola, Emilia!"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Calvin", "French")
+		want := "Bonjour, Calvin!"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
