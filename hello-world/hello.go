f869d413e381986aec6b2499d2ae43ad49418717
maddawik <edwardreillycomputers@gmail.com>
2025-03-17 15:09:10 -0400

    Reduce complexity, extract prefix to its own func
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world#one...last...refactor

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -15,16 +15,20 @@ func Hello(name string, language string) string {
 		name = "world"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name + "!"
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
-	case spanish:
-		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return prefix + name + "!"
+	return
 }
 
 func main() {
