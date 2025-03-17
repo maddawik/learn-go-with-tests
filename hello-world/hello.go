261c5d4d315272fa5c4c496a6b7c611a4e74f11c
maddawik <edwardreillycomputers@gmail.com>
2025-03-17 15:04:27 -0400

    Refactor for French
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world#french

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -4,8 +4,10 @@ import "fmt"
 
 const (
 	spanish            = "Spanish"
+	french             = "French"
 	englishHelloPrefix = "Hello, "
 	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
 )
 
 func Hello(name string, language string) string {
@@ -15,6 +17,9 @@ func Hello(name string, language string) string {
 
 	if language == spanish {
 		return spanishHelloPrefix + name + "!"
+	}
+	if language == french {
+		return frenchHelloPrefix + name + "!"
 	}
 	return englishHelloPrefix + name + "!"
 }
