f1fa1afd013f046fd81d053eb5ef66d47e2c7aed
maddawik <edwardreillycomputers@gmail.com>
2025-03-17 15:01:37 -0400

    Refactor for Spanish requirements
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world#keep-going-more-requirements

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,13 +4,13 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("EJ")
+		got := Hello("EJ", "English")
 		want := "Hello, EJ!"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world!' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, world!"
 		assertCorrectMessage(t, got, want)
 	})
