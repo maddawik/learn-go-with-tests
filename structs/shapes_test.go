3fd6b6cd55978c2ff3fab106444c19eaa3f35ec5
maddawik <edwardreillycomputers@gmail.com>
2025-03-19 15:18:17 -0400

    Refactor
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/structs-methods-and-interfaces#refactor

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,7 +3,8 @@ package main
 import "testing"
 
 func TestPerimiter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,8 +13,9 @@ func TestPerimiter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
+	rectangle := Rectangle{3.0, 8.0}
+	got := Area(rectangle)
+	want := 24.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
