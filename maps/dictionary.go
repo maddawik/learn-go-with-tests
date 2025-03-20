c949bd223133ccf7baff25647995fe551f3e919d
maddawik <edwardreillycomputers@gmail.com>
2025-03-20 16:02:31 -0400

    Green

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,14 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
-	return d[word], nil
+	definition, ok := d[word]
+
+	if !ok {
+		return "", errors.New("this isn't the word you're looking for")
+	}
+	return definition, nil
 }
