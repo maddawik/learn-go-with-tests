68aa0423e5b2fe2fc17de062c29997671b37521c
maddawik <edwardreillycomputers@gmail.com>
2025-03-20 14:24:37 -0400

    Capture reqs for errors on withdraw

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -12,15 +12,26 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(10)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
-
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(10)
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(100)
+
+		assertBalance(t, wallet, Bitcoin(10))
+
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
 	})
 }
