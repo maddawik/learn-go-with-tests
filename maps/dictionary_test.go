6cc4c70de99fcb511f96650478d4e0ee3ca446b7
maddawik <edwardreillycomputers@gmail.com>
2025-03-20 16:06:28 -0400

    Refactor tests, add helper for error check

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,9 +3,8 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "testing things"}
 	t.Run("known word", func(t *testing.T) {
-		dictionary := Dictionary{"test": "testing things"}
-
 		got, _ := dictionary.Search("test")
 		want := "testing things"
 
@@ -13,19 +12,25 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		dictionary := Dictionary{"test": "testing things"}
-
 		_, err := dictionary.Search("metroid")
 
 		if err == nil {
 			t.Fatal("expected to get an error")
 		}
 
-		assertStrings(t, err.Error(), ErrNotFound.Error())
+		assertError(t, err, ErrNotFound)
 	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
 	t.Helper()
 
 	if got != want {
