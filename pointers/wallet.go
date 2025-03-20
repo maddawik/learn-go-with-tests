ea6d38b32a307f565df741fa1fe04b5779975a6d
maddawik <edwardreillycomputers@gmail.com>
2025-03-20 13:44:24 -0400

    Add print statements to illustrate the pass-by-value nature of funcs/methods

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 type Wallet struct {
 	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
 }
 
