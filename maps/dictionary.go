8d5567cbb0e16a737e2de9938685b341283c58d4
maddawik <edwardreillycomputers@gmail.com>
2025-03-20 22:32:03 -0400

    Red

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -2,12 +2,16 @@ package main
 
 import "errors"
 
-var ErrNotFound = errors.New("this isn't the word you're looking for")
+var (
+	ErrNotFound   = errors.New("this isn't the word you're looking for")
+	ErrWordExists = errors.New("you know this word... think")
+)
 
 type Dictionary map[string]string
 
-func (d Dictionary) Add(word, definition string) {
+func (d Dictionary) Add(word, definition string) error {
 	d[word] = definition
+	return nil
 }
 
 func (d Dictionary) Search(word string) (string, error) {
