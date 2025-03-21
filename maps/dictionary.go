058a70dc8f24d8076c29bbb3a67604867e69af76
maddawik <edwardreillycomputers@gmail.com>
2025-03-20 22:59:42 -0400

    Green

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -44,8 +44,19 @@ func (d Dictionary) Update(word, definition string) error {
 	return nil
 }
 
-func (d Dictionary) Delete(word string) {
-	delete(d, word)
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		delete(d, word)
+	default:
+		return err
+	}
+
+	return nil
 }
 
 func (d Dictionary) Search(word string) (string, error) {
