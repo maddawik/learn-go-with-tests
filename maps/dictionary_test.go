0820ace19732235b5a9a13366609ba8ba7d5e3b0
maddawik <edwardreillycomputers@gmail.com>
2025-03-20 22:24:11 -0400

    Capture reqs for throwing an error on existing words

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,13 +3,27 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "metroid"
-	definition := "prime"
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "metroid"
+		definition := "prime"
 
-	dictionary.Add(word, definition)
+		err := dictionary.Add(word, definition)
 
-	assertDefinition(t, dictionary, word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "paper mario"
+		definition := "the origami king"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "the thousand-year door")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func TestSearch(t *testing.T) {
