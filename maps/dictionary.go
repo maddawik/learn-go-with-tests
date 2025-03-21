7f4512b1592b6461a75445edbb93fbc5ff597897
maddawik <edwardreillycomputers@gmail.com>
2025-03-20 21:59:36 -0400

    Red
    Note: We don't have to use a pointer here. This is NOT because maps are
    pass-by-ref in go, it's because a map IS a pointer to a runtime.hmap structure.
    They end up feeling like pass-by-ref in practice.

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -6,6 +6,9 @@ var ErrNotFound = errors.New("this isn't the word you're looking for")
 
 type Dictionary map[string]string
 
+func (d Dictionary) Add(word, definition string) {
+}
+
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 
