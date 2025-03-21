db2e9fd2ca3673101d4be27fc3648f13ac1c5757
maddawik <edwardreillycomputers@gmail.com>
2025-03-20 22:53:15 -0400

    Capture reqs for deleting words/definitions

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -71,6 +71,16 @@ func TestUpdate(t *testing.T) {
 	})
 }
 
+func TestDelete(t *testing.T) {
+	word := "the legend of zelda"
+	dictionary := Dictionary{word: "tears of the kingdom"}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	assertError(t, err, ErrNotFound)
+}
+
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
 	t.Helper()
 
