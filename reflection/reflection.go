898d2e1dbcef46e56de2263ed0f0162ceac0ad8b
maddawik <edwardreillycomputers@gmail.com>
2025-03-25 08:29:24 -0400

    Refactor to check type and then process

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,26 +1,23 @@
 package main
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	if val.Kind() == reflect.Slice {
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := range val.NumField() {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
 		for i := range val.Len() {
 			walk(val.Index(i).Interface(), fn)
 		}
-		return
-	}
-
-	for i := range val.NumField() {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
+	case reflect.String:
+		fn(val.String())
 	}
 }
 
