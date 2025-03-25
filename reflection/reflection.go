aeeed5c170458eb81a2265ff5b4c71a553d2ae4e
maddawik <edwardreillycomputers@gmail.com>
2025-03-25 08:39:15 -0400

    Refactor loop to DRY

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -7,17 +7,22 @@ import (
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
+	numberOfFields := 0
+	var getField func(int) reflect.Value
+
 	switch val.Kind() {
 	case reflect.Struct:
-		for i := range val.NumField() {
-			walk(val.Field(i).Interface(), fn)
-		}
+		numberOfFields = val.NumField()
+		getField = val.Field
 	case reflect.Slice:
-		for i := range val.Len() {
-			walk(val.Index(i).Interface(), fn)
-		}
+		numberOfFields = val.Len()
+		getField = val.Index
 	case reflect.String:
 		fn(val.String())
+	}
+
+	for i := 0; i < numberOfFields; i++ {
+		walk(getField(i).Interface(), fn)
 	}
 }
 
