547378136828faa42f008a740e2e7305fde8be0f
maddawik <edwardreillycomputers@gmail.com>
2025-03-26 15:36:18 -0400

    Add limit for our domain

--- roman-numerals/numerals_test.go
+++ roman-numerals/numerals_test.go
@@ -65,6 +65,10 @@ func TestConvertingToArabic(t *testing.T) {
 
 func TestPropertiesOfConversion(t *testing.T) {
 	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+		t.Log("testing", arabic)
 		roman := ConvertToRoman(arabic)
 		fromRoman := ConvertToArabic(roman)
 		return fromRoman == arabic
