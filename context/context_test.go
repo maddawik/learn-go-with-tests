a0af0dddf194a9aaedce3d8418f95d8434bbee32
maddawik <edwardreillycomputers@gmail.com>
2025-03-26 11:36:19 -0400

    Capture reqs for a context to cancel

--- context/context_test.go
+++ context/context_test.go
@@ -1,29 +1,43 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 type SpyStore struct {
-	response string
+	response  string
+	cancelled bool
 }
 
 func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
 }
 
 func TestServer(t *testing.T) {
 	data := "hello, world"
-	svr := Server(&SpyStore{data})
+	store := &SpyStore{response: data}
+	svr := Server(store)
 
 	request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	cancellingCtx, cancel := context.WithCancel(request.Context())
+	time.AfterFunc(5*time.Millisecond, cancel)
+	request = request.WithContext(cancellingCtx)
+
 	response := httptest.NewRecorder()
 
 	svr.ServeHTTP(response, request)
 
-	if response.Body.String() != data {
-		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+	if !store.cancelled {
+		t.Error("store was not told to cancel")
 	}
 }
