c5262408e3db276d6e453808bc116f89954b30ac
maddawik <edwardreillycomputers@gmail.com>
2025-03-26 15:03:47 -0400

    (Red) Capture reqs for remaining roman numerals

--- roman-numerals/numerals_test.go
+++ roman-numerals/numerals_test.go
@@ -6,12 +6,12 @@ func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
 		Description string
 		Arabic      int
-		Want        string
+		Roman       string
 	}{
 		{"1 gets converted to I", 1, "I"},
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
-		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"4 gets converted to IV", 4, "IV"},
 		{"5 gets converted to V", 5, "V"},
 		{"6 gets converted to VI", 6, "VI"},
 		{"7 gets converted to VII", 7, "VII"},
@@ -26,13 +26,24 @@ func TestRomanNumerals(t *testing.T) {
 		{"47 gets converted to XLVII", 47, "XLVII"},
 		{"49 gets converted to XLIX", 49, "XLIX"},
 		{"50 gets converted to L", 50, "L"},
+		{"90 gets converted to XC", 90, "XC"},
+		{"100 gets converted to C", 100, "C"},
+		{"400 gets converted to CD", 400, "CD"},
+		{"500 gets converted to D", 500, "D"},
+		{"900 gets converted to CM", 900, "CM"},
+		{"1000 gets converted to M", 1000, "M"},
+		{"1984 gets converted to MCMLXXXIV", 1984, "MCMLXXXIV"},
+		{"3999 gets converted to MMMCMXCIX", 3999, "MMMCMXCIX"},
+		{"2014 gets converted to MMXIV", 2014, "MMXIV"},
+		{"1006 gets converted to MVI", 1006, "MVI"},
+		{"798 gets converted to DCCXCVIII", 798, "DCCXCVIII"},
 	}
 
 	for _, test := range cases {
 		t.Run(test.Description, func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
-			if got != test.Want {
-				t.Errorf("got %q, want %q", got, test.Want)
+			if got != test.Roman {
+				t.Errorf("got %q, want %q", got, test.Roman)
 			}
 		})
 	}
