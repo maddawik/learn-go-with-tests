257f655d9aae7478bf59de7e1efe3cdfbafc5e55
maddawik <edwardreillycomputers@gmail.com>
2025-03-27 17:17:23 -0400

    EXTRA: (Green) Check for md files only

--- reading-files/blogposts.go
+++ reading-files/blogposts.go
@@ -3,7 +3,10 @@ package blogposts
 import (
 	"errors"
 	"io/fs"
+	"strings"
 )
+
+var ErrNoMarkdownFiles = errors.New("no markdown files in directory")
 
 func NewPostsFromFS(filesystem fs.FS) ([]Post, error) {
 	dir, err := fs.ReadDir(filesystem, ".")
@@ -11,12 +14,13 @@ func NewPostsFromFS(filesystem fs.FS) ([]Post, error) {
 		return nil, err
 	}
 
-	if len(dir) == 0 {
-		return nil, errors.New("no posts to parse in directory")
+	mdFiles, err := getMarkdownFiles(dir)
+	if err != nil {
+		return nil, err
 	}
 
 	var posts []Post
-	for _, f := range dir {
+	for _, f := range mdFiles {
 		post, err := getPost(filesystem, f.Name())
 		if err != nil {
 			return nil, err
@@ -33,4 +37,19 @@ func getPost(filesystem fs.FS, filename string) (Post, error) {
 	}
 	defer postFile.Close()
 	return newPost(postFile)
+}
+
+func getMarkdownFiles(dir []fs.DirEntry) ([]fs.DirEntry, error) {
+	result := make([]fs.DirEntry, 0)
+	for _, f := range dir {
+		if strings.HasSuffix(f.Name(), ".md") {
+			result = append(result, f)
+		}
+	}
+
+	if len(result) == 0 {
+		return nil, ErrNoMarkdownFiles
+	}
+
+	return result, nil
 }
