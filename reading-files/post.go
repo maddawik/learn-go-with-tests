1a1e7bb95a2b8c18bbde1a20d6e1b064bc2c70c6
maddawik <edwardreillycomputers@gmail.com>
2025-03-27 14:34:06 -0400

    (Green) Get the body

--- reading-files/post.go
+++ reading-files/post.go
@@ -2,6 +2,8 @@ package blogposts
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 	"strings"
 )
@@ -27,9 +29,24 @@ func newPost(postFile io.Reader) (Post, error) {
 		return strings.TrimPrefix(scanner.Text(), tagName)
 	}
 
+	title := readMetaLine(titleSeparator)
+	description := readMetaLine(descriptionSeparator)
+	tags := strings.Split(readMetaLine(tagSeparator), ", ")
+
+	scanner.Scan() // Ignore the `---`
+
+	buf := bytes.Buffer{}
+
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+
+	body := strings.TrimSuffix(buf.String(), "\n")
+
 	return Post{
-		Title:       readMetaLine(titleSeparator),
-		Description: readMetaLine(descriptionSeparator),
-		Tags:        strings.Split(readMetaLine(tagSeparator), ", "),
+		Title:       title,
+		Description: description,
+		Tags:        tags,
+		Body:        body,
 	}, nil
 }
