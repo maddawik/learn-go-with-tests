0245686c7b8bb79155a21c74a00953df5f8b2c62
maddawik <edwardreillycomputers@gmail.com>
2025-03-27 13:58:53 -0400

    Refactor things
    Move Post struct and newPost into it's own file

--- reading-files/blogposts.go
+++ reading-files/blogposts.go
@@ -1,13 +1,8 @@
 package blogposts
 
 import (
-	"io"
 	"io/fs"
 )
-
-type Post struct {
-	Title string
-}
 
 func NewPostsFromFS(filesystem fs.FS) ([]Post, error) {
 	dir, err := fs.ReadDir(filesystem, ".")
@@ -17,7 +12,7 @@ func NewPostsFromFS(filesystem fs.FS) ([]Post, error) {
 
 	var posts []Post
 	for _, f := range dir {
-		post, err := getPost(filesystem, f)
+		post, err := getPost(filesystem, f.Name())
 		if err != nil {
 			return nil, err
 		}
@@ -26,18 +21,11 @@ func NewPostsFromFS(filesystem fs.FS) ([]Post, error) {
 	return posts, nil
 }
 
-func getPost(filesystem fs.FS, f fs.DirEntry) (Post, error) {
-	postFile, err := filesystem.Open(f.Name())
+func getPost(filesystem fs.FS, filename string) (Post, error) {
+	postFile, err := filesystem.Open(filename)
 	if err != nil {
 		return Post{}, err
 	}
 	defer postFile.Close()
-
-	postData, err := io.ReadAll(postFile)
-	if err != nil {
-		return Post{}, err
-	}
-
-	post := Post{Title: string(postData)[7:]}
-	return post, nil
+	return newPost(postFile)
 }
