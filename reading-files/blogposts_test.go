dabfe1fefaee0de47275449d584605a0a9b34504
maddawik <edwardreillycomputers@gmail.com>
2025-03-27 13:59:30 -0400

    (Red) Capture reqs for parsing description

--- reading-files/blogposts_test.go
+++ reading-files/blogposts_test.go
@@ -9,9 +9,15 @@ import (
 )
 
 func TestBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1`
+		secondBody = `Title: Post 2
+Description: Description 2`
+	)
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("Title: Post 1")},
-		"hello-world2.md": {Data: []byte("Title: Post 2")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
 	posts, err := blogposts.NewPostsFromFS(fs)
@@ -19,7 +25,10 @@ func TestBlogPosts(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	assertPosts(t, posts[0], blogposts.Post{Title: "Post 1"})
+	assertPosts(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+	})
 }
 
 func assertPosts(t testing.TB, got blogposts.Post, want blogposts.Post) {
