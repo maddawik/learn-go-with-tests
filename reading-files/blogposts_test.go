42e5a9daeca052545e20c3b7a145cebce4a6abb7
maddawik <edwardreillycomputers@gmail.com>
2025-03-27 16:37:55 -0400

    EXTRA: (Red) Capture reqs for throwing an error on empty fs tree

--- reading-files/blogposts_test.go
+++ reading-files/blogposts_test.go
@@ -9,44 +9,56 @@ import (
 )
 
 func TestBlogPosts(t *testing.T) {
-	const (
-		firstBody = `Title: Post 1
+	t.Run("fs with 2 valid files", func(t *testing.T) {
+		const (
+			firstBody = `Title: Post 1
 Description: Description 1
 Tags: go, tdd
 ---
 Hello
 World`
 
-		secondBody = `Title: Post 2
+			secondBody = `Title: Post 2
 Description: Description 2
 Tags: rust, borrow-checker
 ---
 Hola
 Enrique`
-	)
-	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte(firstBody)},
-		"hello-world2.md": {Data: []byte(secondBody)},
-	}
+		)
+		fs := fstest.MapFS{
+			"hello world.md":  {Data: []byte(firstBody)},
+			"hello-world2.md": {Data: []byte(secondBody)},
+		}
 
-	posts, err := blogposts.NewPostsFromFS(fs)
-	if err != nil {
-		t.Fatal(err)
-	}
+		posts, err := blogposts.NewPostsFromFS(fs)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	assertPosts(t, posts[0], blogposts.Post{
-		Title:       "Post 1",
-		Description: "Description 1",
-		Tags:        []string{"go", "tdd"},
-		Body: `Hello
+		assertPosts(t, posts[0], blogposts.Post{
+			Title:       "Post 1",
+			Description: "Description 1",
+			Tags:        []string{"go", "tdd"},
+			Body: `Hello
 World`,
-	})
-	assertPosts(t, posts[1], blogposts.Post{
-		Title:       "Post 2",
-		Description: "Description 2",
-		Tags:        []string{"rust", "borrow-checker"},
-		Body: `Hola
+		})
+		assertPosts(t, posts[1], blogposts.Post{
+			Title:       "Post 2",
+			Description: "Description 2",
+			Tags:        []string{"rust", "borrow-checker"},
+			Body: `Hola
 Enrique`,
+		})
+	})
+
+	t.Run("fs with no files returns error", func(t *testing.T) {
+		fs := fstest.MapFS{}
+
+		_, err := blogposts.NewPostsFromFS(fs)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
 	})
 }
 
