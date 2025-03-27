e4fb53ebcecd67f7ad091f74a423ac3ff4dc064a
maddawik <edwardreillycomputers@gmail.com>
2025-03-27 11:12:59 -0400

    Capture reqs for reading blog posts from a filesystem
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/reading-files

---
+++ reading-files/blogposts_test.go
@@ -0,0 +1,21 @@
+package blogposts_test
+
+import (
+	"testing"
+	"testing/fstest"
+
+	blogposts "github.com/maddawik/learn-go-with-tests/reading-files"
+)
+
+func TestBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("hey")},
+		"hello-world2.md": {Data: []byte("bonjour")},
+	}
+
+	posts := blogposts.NewPostFromFS(fs)
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d, wanted %d posts", len(posts), len(fs))
+	}
+}
