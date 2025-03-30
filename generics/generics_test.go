6ffb6d08529d8ae7f2ef72d4a94fc878cfb2ecbf
maddawik <edwardreillycomputers@gmail.com>
2025-03-30 14:23:11 -0400

    (Red): Capture reqs for an integer stack

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -16,3 +16,22 @@ func TestAssertFunctions(t *testing.T) {
 		AssertFalse(t, false)
 	})
 }
+
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+		myStackOfInts := new(StackOfInts)
+
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(123)
+
+		AssertFalse(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(456)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 456)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 123)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+	})
+}
