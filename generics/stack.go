31d3d97667376c9a936962fa440b9e9793be9e8b
maddawik <edwardreillycomputers@gmail.com>
2025-03-30 15:03:00 -0400

    (Green): Implement string stack... not very DRY though

--- generics/stack.go
+++ generics/stack.go
@@ -23,3 +23,27 @@ func (s *StackOfInts) Pop() (int, bool) {
 func (s *StackOfInts) IsEmpty() bool {
 	return len(s.values) == 0
 }
+
+type StackOfStrings struct {
+	values []string
+}
+
+func (s *StackOfStrings) Push(value string) {
+	s.values = append(s.values, value)
+}
+
+func (s *StackOfStrings) Pop() (string, bool) {
+	if s.IsEmpty() {
+		return "", false
+	}
+
+	index := len(s.values) - 1
+	result := s.values[index]
+	s.values = s.values[:index]
+
+	return result, true
+}
+
+func (s *StackOfStrings) IsEmpty() bool {
+	return len(s.values) == 0
+}
