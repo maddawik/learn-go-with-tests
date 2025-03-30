8dd31a8b41ed9440ccdce9f0657595a692232857
maddawik <edwardreillycomputers@gmail.com>
2025-03-29 21:05:39 -0400

    (Green): Add Assertion helper functions
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/generics

---
+++ generics/generics_test.go
@@ -0,0 +1,28 @@
+package generics
+
+import "testing"
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "hey")
+	})
+}
+
+func AssertEqual(t *testing.T, got, want any) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func AssertNotEqual(t *testing.T, got, want any) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %d", got)
+	}
+}
