847124e7c7461f127391070b090c7ffaa5ab20a2
maddawik <edwardreillycomputers@gmail.com>
2025-03-29 21:22:18 -0400

    (Green): Refactor helpers to use generics instead of interface{}

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -13,16 +13,16 @@ func TestAssertFunctions(t *testing.T) {
 	})
 }
 
-func AssertEqual(t *testing.T, got, want any) {
+func AssertEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %d, want %d", got, want)
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
 
-func AssertNotEqual(t *testing.T, got, want any) {
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got == want {
-		t.Errorf("didn't want %d", got)
+		t.Errorf("didn't want %+v", got)
 	}
 }
