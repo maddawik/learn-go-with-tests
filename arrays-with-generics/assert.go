94f9123c6aa70c5470d32896a1deab355df4eda6
maddawik <edwardreillycomputers@gmail.com>
2025-03-31 16:01:51 -0400

    Add helper assertion functions

---
+++ arrays-with-generics/assert.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+func AssertEqual(t *testing.T, got, want any) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
+
+func AssertNotEqual(t *testing.T, got, want any) {
+	t.Helper()
+	if got == want {
+		t.Errorf("didn't want %+v", got)
+	}
+}
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Error("expected true")
+	}
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Error("expected true")
+	}
+}
