a3cce48e25bfecce3e6ab019021141e98ca9b857
maddawik <edwardreillycomputers@gmail.com>
2025-03-31 16:52:01 -0400

    (Red): Capture reqs for an extended bad bank

--- arrays-with-generics/bad_bank_test.go
+++ arrays-with-generics/bad_bank_test.go
@@ -3,20 +3,20 @@ package main
 import "testing"
 
 func TestBadBank(t *testing.T) {
+	joe := Account{Name: "Joe", Balance: 100}
+	jane := Account{Name: "Jane", Balance: 75}
+	jim := Account{Name: "Jim", Balance: 200}
+
 	transactions := []Transaction{
-		{
-			From: "Joe",
-			To:   "Jane",
-			Sum:  100,
-		},
-		{
-			From: "Jim",
-			To:   "Joe",
-			Sum:  25,
-		},
+		NewTransaction(jane, joe, 100),
+		NewTransaction(jim, jane, 25),
 	}
 
-	AssertEqual(t, BalanceFor(transactions, "Jane"), 100.0)
-	AssertEqual(t, BalanceFor(transactions, "Joe"), -75.0)
-	AssertEqual(t, BalanceFor(transactions, "Jim"), -25.0)
+	newBalanceFor := func(account Account) float64 {
+		return NewBalanceFor(account, transactions).Balance
+	}
+
+	AssertEqual(t, newBalanceFor(joe), 200.0)
+	AssertEqual(t, newBalanceFor(jane), 0.0)
+	AssertEqual(t, newBalanceFor(jim), 175.0)
 }
