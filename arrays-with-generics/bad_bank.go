9ca1686f86166518f8cbeb22deecbb3f2dd59d31
maddawik <edwardreillycomputers@gmail.com>
2025-03-31 16:29:11 -0400

    (Green): Implement BalanceFor (without generics)

--- arrays-with-generics/bad_bank.go
+++ arrays-with-generics/bad_bank.go
@@ -6,6 +6,15 @@ type Transaction struct {
 	Sum  float64
 }
 
-func BalanceFor(transactions []Transaction, name string) int {
-	return 0
+func BalanceFor(transactions []Transaction, name string) float64 {
+	var balance float64
+	for _, transaction := range transactions {
+		if transaction.From == name {
+			balance -= transaction.Sum
+		}
+		if transaction.To == name {
+			balance += transaction.Sum
+		}
+	}
+	return balance
 }
