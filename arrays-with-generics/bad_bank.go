0f9d53970aebbcb53357db863016d22d370dbdd4
maddawik <edwardreillycomputers@gmail.com>
2025-03-31 17:02:22 -0400

    (Green): Implement Reduce for applying transactions to an account

--- arrays-with-generics/bad_bank.go
+++ arrays-with-generics/bad_bank.go
@@ -1,9 +1,32 @@
 package main
 
+type Account struct {
+	Name    string
+	Balance float64
+}
+
 type Transaction struct {
 	From string
 	To   string
 	Sum  float64
+}
+
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(transactions, applyTransaction, account)
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if transaction.From == a.Name {
+		a.Balance -= transaction.Sum
+	}
+	if transaction.To == a.Name {
+		a.Balance += transaction.Sum
+	}
+	return a
 }
 
 func BalanceFor(transactions []Transaction, name string) float64 {
