84d6ea4bea0502f6c9a582d5846105b74bd9bd05
maddawik <edwardreillycomputers@gmail.com>
2025-03-31 10:28:35 -0400

    Copy arrays folder to new generics version
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/revisiting-arrays-and-slices-with-generics

---
+++ arrays-with-generics/sum.go
@@ -0,0 +1,53 @@
+package main
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
+
+func SumAllHeads(numbersToSum ...[]int) int {
+	var sum int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			continue
+		}
+		sum += numbers[0]
+	}
+
+	return sum
+}
+
+func Reduce[A any](collection []A, f func(A, A) A, initialValue A) A {
+	result := initialValue
+	for _, v := range collection {
+		result = f(result, v)
+	}
+	return result
+}
---
+++ arrays-with-generics/sum_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d, want %d given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of varying size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d, want %d given %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum of empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 6})
+		want := []int{0, 10}
+		checkSums(t, got, want)
+	})
+}
+
+func TestSumAllHeads(t *testing.T) {
+	checkSums := func(t testing.TB, got, want int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	}
+	t.Run("gets the sum of the head of some slices", func(t *testing.T) {
+		got := SumAllHeads([]int{1, 2}, []int{5, 6})
+		want := 6
+		checkSums(t, got, want)
+	})
+
+	t.Run("sums empty slices", func(t *testing.T) {
+		got := SumAllHeads([]int{}, []int{20, 30})
+		want := 20
+		checkSums(t, got, want)
+	})
+}
