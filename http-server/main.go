a5e8f084e6f5070c2bd5c9f7bbf6622ea92f1ecc
maddawik <edwardreillycomputers@gmail.com>
2025-04-01 16:24:37 -0400

    (Green): Refactor and implement in memory store

--- http-server/main.go
+++ http-server/main.go
@@ -5,15 +5,6 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (s *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-func (s *InMemoryPlayerStore) RecordWin(name string) {
-}
-
 func main() {
 	server := &PlayerServer{store: &InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5000", server))
