d1d444aa2a9228f367effaf9e61abfe88667bcf9
maddawik <edwardreillycomputers@gmail.com>
2025-04-01 16:01:10 -0400

    (Red): Get main running with InMemory store

--- http-server/main.go
+++ http-server/main.go
@@ -11,6 +11,9 @@ func (s *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	return 123
 }
 
+func (s *InMemoryPlayerStore) RecordWin(name string) {
+}
+
 func main() {
 	server := &PlayerServer{store: &InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5000", server))
