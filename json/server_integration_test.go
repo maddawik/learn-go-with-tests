855baecc1a9c0743c0024f44c3db587e882a32c8
maddawik <edwardreillycomputers@gmail.com>
2025-04-02 15:23:18 -0400

    (Red): Add integration test for league

--- json/server_integration_test.go
+++ json/server_integration_test.go
@@ -15,8 +15,24 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 
-	response := httptest.NewRecorder()
-	server.ServeHTTP(response, newGetScoreRequest(player))
-	assertStatus(t, response.Code, http.StatusOK)
-	assertResponseBody(t, response.Body.String(), "3")
+	t.Run("get score", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newGetScoreRequest(player))
+		assertStatus(t, response.Code, http.StatusOK)
+
+		assertResponseBody(t, response.Body.String(), "3")
+	})
+
+	t.Run("get league", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newLeagueRequest())
+
+		got := getLeagueFromResponse(t, response.Body)
+		want := []Player{
+			{Name: player, Wins: 3},
+		}
+
+		assertStatus(t, response.Code, http.StatusOK)
+		assertLeague(t, got, want)
+	})
 }
