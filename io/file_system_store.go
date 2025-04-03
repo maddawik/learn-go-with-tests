fc9daa15e72c76bbaf91541ff9d9a77e73d5f26b
maddawik <edwardreillycomputers@gmail.com>
2025-04-03 17:05:57 -0400

    (Green): Handle an empty file

--- io/file_system_store.go
+++ io/file_system_store.go
@@ -14,6 +14,17 @@ type FileSystemPlayerStore struct {
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 	file.Seek(0, io.SeekStart)
+
+	info, err := file.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("problem getting the info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, io.SeekStart)
+	}
+
 	league, err := NewLeague(file)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't create new player store from file %s, %v", file.Name(), err)
