85f585b43da9530cd9f1d4eaaa0d07e0133d5389
maddawik <edwardreillycomputers@gmail.com>
2025-04-03 17:45:24 -0400

    (Green): Refactor file initialization on fsplayterstore constructor

--- io/file_system_store.go
+++ io/file_system_store.go
@@ -13,16 +13,9 @@ type FileSystemPlayerStore struct {
 }
 
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, io.SeekStart)
-
-	info, err := file.Stat()
+	err := initializePlayerDBFile(file)
 	if err != nil {
-		return nil, fmt.Errorf("problem getting the info from file %s, %v", file.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		file.Write([]byte("[]"))
-		file.Seek(0, io.SeekStart)
+		return nil, fmt.Errorf("couldn't initialize player db from file %s, %v", file.Name(), err)
 	}
 
 	league, err := NewLeague(file)
@@ -33,6 +26,21 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}, nil
+}
+
+func initializePlayerDBFile(file *os.File) error {
+	file.Seek(0, io.SeekStart)
+
+	info, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("problem getting the info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, io.SeekStart)
+	}
+	return nil
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
