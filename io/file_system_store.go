5c9c479ca2c657641c595fd2de00a323aae2e6bd
maddawik <edwardreillycomputers@gmail.com>
2025-04-03 14:12:16 -0400

    (Green): Minimal code to get tests passing
    https://quii.gitbook.io/learn-go-with-tests/build-an-application/io#write-enough-code-to-make-it-pass-2

--- io/file_system_store.go
+++ io/file_system_store.go
@@ -30,6 +30,17 @@ func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
 }
 
 func (f *FileSystemPlayerStore) RecordWin(name string) {
+	league := f.GetLeague()
+
+	for i, player := range league {
+		if player.Name == name {
+			league[i].Wins++
+			break
+		}
+	}
+
+	f.database.Seek(0, io.SeekStart)
+	json.NewEncoder(f.database).Encode(league)
 }
 
 func NewLeague(rdr io.Reader) ([]Player, error) {
