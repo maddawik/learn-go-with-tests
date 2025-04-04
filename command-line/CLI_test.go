d9aa43910b625c21b23308dfee3a11f6ce6c465f
maddawik <edwardreillycomputers@gmail.com>
2025-04-04 13:57:01 -0400

    (Red): Capture reqs reading input in CLI

--- command-line/CLI_test.go
+++ command-line/CLI_test.go
@@ -1,13 +1,25 @@
 package poker
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCLI(t *testing.T) {
+	in := strings.NewReader("Jeff wins\n")
 	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore}
+
+	cli := &CLI{playerStore, in}
 	cli.PlayPoker()
 
 	if len(playerStore.winCalls) != 1 {
 		t.Error("expected a win call but didn't get any")
+	}
+
+	got := playerStore.winCalls[0]
+	want := "Jeff"
+
+	if got != want {
+		t.Errorf("didn't record correct winner, got %q want %q", got, want)
 	}
 }
