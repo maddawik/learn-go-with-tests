e1539e556cdd92ea8ba7242dbde7e3da9de02ef2
maddawik <edwardreillycomputers@gmail.com>
2025-04-09 09:37:09 -0400

    (Green): Refactor game request to helper

--- websockets/server_test.go
+++ websockets/server_test.go
@@ -94,13 +94,18 @@ func TestGame(t *testing.T) {
 	t.Run("GET /game returns 200", func(t *testing.T) {
 		server := NewPlayerServer(&StubPlayerStore{})
 
-		request, _ := http.NewRequest(http.MethodGet, "/game", nil)
+		request := NewGameRequest()
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
 		AssertStatus(t, response.Code, http.StatusOK)
 	})
+}
+
+func NewGameRequest() *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, "/game", nil)
+	return request
 }
 
 func NewLeagueRequest() *http.Request {
